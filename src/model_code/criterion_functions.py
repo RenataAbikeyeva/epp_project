d9f41e059897bfcddcf600eaf139d3b92088d664
Renata Abikeyeva <renata.abikeyeva@gmail.com>
2020-02-22 16:38:53 +0100

    Add Sum of Squares Parameters Version 2.

--- src/model_code/criterion_functions.py
+++ src/model_code/criterion_functions.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 
-params = pd.DataFrame([1, 2, 3], columns=["value"])
-
 
 def sum_of_squares_v1(params):
     """ Implement Sum of Squared Parameters function.
@@ -17,10 +15,26 @@ def sum_of_squares_v1(params):
         integer:  Sum of Squared Parameters function output.
 
     """
-    params["indexer"] = np.arange(
-        1, len(params.index) + 1
-    )  # what if params has a multi-index?
+    params["indexer"] = np.arange(1, len(params.index) + 1)
     return (params["indexer"] * params["value"] ** 2).sum()
+
+
+def sum_of_squares_v2(params):
+    """ Implement Sum of Squared Parameters function.
+
+    Function description: https://www.sfu.ca/~ssurjano/sumsqu.html.
+
+    Args:
+        params (pandas.DataFrame): Must have the column "value" containing
+        input values for parameters. Accepts arbitrary numbers of input values.
+
+    Returns:
+        integer: Sum of Squared Parameters function output.
+
+    """
+    val = pd.array([(i + 1) * i_val ** 2 for i, i_val in enumerate(params["value"])])
+
+    return val.sum()
 
 
 def trid(params):
