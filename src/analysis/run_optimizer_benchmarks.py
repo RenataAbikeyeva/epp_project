0b75e7840a2dac5f61a04709e5661910da444887
Renata Abikeyeva <renata.abikeyeva@gmail.com>
2020-02-28 15:01:39 +0100

    Update run_optimizer_benchmarks.py. Works for 11 optimizers (pygmo causing trouble).

--- src/analysis/run_optimizer_benchmarks.py
+++ src/analysis/run_optimizer_benchmarks.py
@@ -10,9 +10,6 @@ from estimagic.optimization.optimize import minimize
 
 # from src.model_code.criterion_functions import sum_of_squares_v1
 
-index = pd.Index(["x_1", "x_2", "x_3"], name="parameters")
-start_params = pd.DataFrame(index=index)
-start_params["value"] = ""
 
 constraints = [
     [],
@@ -27,26 +24,54 @@ constraints = [
     [{"loc": ["x_1", "x_2"], "type": "linear", "weights": [1, 2], "value": 4}],
 ]
 
-optimizers = ["scipy_L-BFGS-B", "scipy_SLSQP"]
-# optimizers = ["scipy_L-BFGS-B", "scipy_SLSQP", "scipy_TNC", "nlopt_auglag_eq",
-# "nlopt_auglag", "nlopt_newuoa", "nlopt_newuoa_bound", "nlopt_praxis",
-# "nlopt_neldermead", "nlopt_sbplx"]
+algorithms = [
+    "scipy_SLSQP",
+    "scipy_TNC",
+    "scipy_L-BFGS-B",
+    "nlopt_auglag_eq",
+    "nlopt_auglag",
+    "nlopt_newuoa",
+    "nlopt_newuoa_bound",
+    "nlopt_neldermead",
+    "nlopt_sbplx" "nlopt_cobyla",
+    "nlopt_bobyqa",
+]
 
 criteria = [sum_of_squares_v1, trid, rotated_hyper_ellipsoid, rosenbrock_v2]
 
 # run the actual benchmark
 results = []
-for opt in optimizers:
+for alg in algorithms:
+    origin, algo_name = alg.split("_", 1)
+    if origin == "pygmo":
+        if algo_name in ["ihs"]:
+            algo_options = {"popsize": 1, "gen": 1000}
+        elif algo_name in ["sea"]:
+            algo_options = {"popsize": 5, "gen": 7000}
+        else:
+            algo_options = {"popsize": 30, "gen": 150}
+    else:
+        algo_options = {}
+
     for constr in constraints:
+        index = pd.Index(["x_1", "x_2", "x_3"], name="parameters")
+        start_params = pd.DataFrame(index=index)
+        start_params["value"] = ""
+        start_params["lower"] = -5
+        start_params["upper"] = 5
+
         if constr == []:
             start_params["value"] = [1, 2, 3]
         if constr == constraints[1]:
             start_params["value"] = [1, 3, 2]
         if constr == constraints[2]:
+            start_params = pd.DataFrame(index=index)
             start_params["value"] = [0.5, 0.5, 3]
         if constr == constraints[3]:
+            start_params = pd.DataFrame(index=index)
             start_params["value"] = [1, 2, 3]
         if constr == constraints[4]:
+            start_params = pd.DataFrame(index=index)
             start_params["value"] = [2, 1, 3]
         if constr == constraints[5]:
             start_params["value"] = [2, 2, 2]
@@ -57,13 +82,18 @@ for opt in optimizers:
         if constr == constraints[8]:
             start_params["value"] = [1, 1, 1]
         if constr == constraints[9]:
+            start_params = pd.DataFrame(index=index)
             start_params["value"] = [2, 1, 3]
         for crit in criteria:
             info, opt_params = minimize(
-                crit, start_params, constraints=constr, algorithm=opt
+                crit,
+                start_params,
+                constraints=constr,
+                algorithm=alg,
+                algo_options=algo_options,
             )
 
-            opt_params["algorithm"] = opt
+            opt_params["algorithm"] = alg
             opt_params["constraints"] = str(constr)
             opt_params["criterion"] = crit.__name__
             opt_params.reset_index(inplace=True)
@@ -76,6 +106,4 @@ for opt in optimizers:
 
 df = pd.concat(results, sort=False)
 df.reset_index(inplace=True, drop=True)
-# df.to_pickle("test_crit_constr_2opti.pickle")
-# df = pd.read_pickle("test_crit_constr_2opti.pickle")
-df.to_csv("2optimizers.csv")
+df.to_csv("11optimizers.csv", index=False)
